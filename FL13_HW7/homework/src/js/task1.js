9dde83bbdf2f72b6a3c376339f376b8d0dd117c3
Stanislav Sukhanov <sukhanov.stas@gmail.com>
2020-04-27 23:52:29 +0300

    task 1 is ready

---
+++ FL13_HW7/homework/src/js/task1.js
@@ -0,0 +1,50 @@
+// Your code goes here
+const users = [
+    {name: 'Admin', pass: 'RootPass'},
+    {name: 'User', pass: 'UserPass'},
+]
+
+const userName = prompt('Please, enter your user name:');
+
+// main logic
+if (inputInvalid(userName)) {
+    cancelTransaction()
+} else if (userName.trim().length < 4) {
+    alert('I don\'t know any users having name length less than 4 symbols');
+} else if (userNotExist()) {
+    alert('I don’t know you');
+} else {
+    const user = getUser();
+    const passwordInput = prompt('Enter your password');
+    if (inputInvalid(passwordInput)) {
+        cancelTransaction();
+    } else {
+        if (passwordInput !== user.pass) {
+            alert('Wrong password');
+        } else {
+            const userHours = new Date().getHours();
+            const userTime = userHours < 20? 'day' : 'evening';
+            alert(`Good ${userTime}, dear ${user.name}!`)
+        }
+    }
+}
+
+// functions
+function inputInvalid(str) {
+    return !str;
+}
+
+function cancelTransaction(){
+    alert('Cancelled')
+}
+
+function getUser() {
+    return users.find(user => user.name === userName.trim());
+}
+
+function userNotExist() {
+    return !getUser();
+}
+
+
+
